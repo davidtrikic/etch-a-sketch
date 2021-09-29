c18689f774279faa94e9868a1be13fec4815316e
david <davidtrikic@outlook.com>
2021-09-29 17:38:07 +0200

    add paint and brushColor function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sketchPad = document.getElementById('sketchpad');
 const gridSizeInput = document.getElementById('grid-size');
-
-
+const brushColorInput = document.getElementById('color');
+const root = document.documentElement;
 
 window.onload = createGrid;
 
@@ -10,21 +10,44 @@ function createGrid() {
 	let square, i;
 
 	clearGrid();
-	sketchpad.setAttribute('class', `sketchpad sketchpad-${gridSizeInput.value}`);
+
+	// Set CSS grid size according to input value
+	root.style.setProperty('--grid-size', `repeat(${gridSizeInput.value}, minmax(9px, 72px))`);
 
 	for (i = 0; i < gridSizeInput.value ** 2; i++) {
 		square = document.createElement('div');
-		square.setAttribute('class', `grid-square grid-square${gridSizeInput.value}`);
+		square.setAttribute('class', `grid-square`);
 		sketchpad.appendChild(square);
+
+		square.addEventListener('mousemove', paintSquare);
 	}
 }
-
+// Change grid size on input element
 gridSizeInput.addEventListener('change', function() {
 	createGrid();
 })
 
+
+let brushColor = brushColorInput.value; // Get initial brush color
+brushColorInput.addEventListener('change', getBrushcolor);
+
+
+function getBrushcolor() {
+	brushColor = brushColorInput.value;
+}
+
+
+// Empty grid brefore creating a new one
 function clearGrid() {
 	while (sketchpad.firstChild) {
 		sketchpad.removeChild(sketchpad.firstChild);
 	}
 }
+
+function paintSquare(e) {
+	// Paint div if mouse button is pressed
+	if (e.buttons == 1) {
+		e.target.style.background = brushColor;
+	}
+}
+
